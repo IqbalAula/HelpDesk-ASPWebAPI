6ed51b38adda0f55b99cafe91360f43f6af6d242
Ilham Habibie <habibiilham12@gmail.com>
2019-02-15 18:03:00 +0700

    push ilham fix manage Province

--- HelpDesk.API/HelpDesk.API/App_Start/UnityConfig.cs
+++ HelpDesk.API/HelpDesk.API/App_Start/UnityConfig.cs
@@ -1,3 +1,7 @@
+using HelpDesk.BussinessLogic.Interface;
+using HelpDesk.BussinessLogic.Interface.Master;
+using HelpDesk.Common.Interface;
+using HelpDesk.Common.Interface.Master;
 using System.Web.Http;
 using Unity;
 using Unity.WebApi;
@@ -9,12 +13,17 @@ namespace HelpDesk.API
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+            //this is service area
+            container.RegisterType<IProvinceService, ProvinceService>();
+
+            //this is repository area
+            container.RegisterType<IProvinceRepository, ProvinceRepository>();
+
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
