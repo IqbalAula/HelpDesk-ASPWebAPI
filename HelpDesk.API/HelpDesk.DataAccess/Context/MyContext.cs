42b5c81e54d145516e5e7b57936437169a359a02
Ilham Habibie <habibiilham12@gmail.com>
2019-02-15 16:41:20 +0700

    push ilham

---
+++ HelpDesk.API/HelpDesk.DataAccess/Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using HelpDesk.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.DataAccess.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("MyContext") { }
+        public DbSet<Province> Proviences { get; set; }
+        public DbSet<Regency> Regencies { get; set; }
+    }
+}
